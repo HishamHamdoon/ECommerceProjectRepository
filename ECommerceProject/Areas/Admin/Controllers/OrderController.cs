097f610bf32ecf9a848affcfb7dc5dffcec654c1
هشام آدم <hamdon_22@hotmail.com>
2024-02-13 23:45:17 +0300

    add order controller and index view

---
+++ ECommerceProject/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Ecom.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceProject.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+			_unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region api calls
+		public IActionResult GetAll()
+		{
+			var orderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+			return Json(new { data = orderList });
+		}
+		
+		#endregion
+	}
+}
---
+++ ECommerceProject/wwwroot/js/order.js
@@ -0,0 +1,59 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {url: '/admin/order/GetAll'},
+        "columns": [
+            {data:'id',"width":"15%"},
+            { data:'name',"width":"15%"},
+            {data:'applicationUser.email',"width":"15%"},
+            {data:'phoneNumber',"width":"15%"},
+            {data:'orderStatus',"width":"15%"},
+            {data:'orderTotal',"width":"15%"},
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `
+                    <div class="w-75 btn-group" role="group">
+                    <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2">
+                <i class="bi bi-pencil"></i>
+            </a>
+           
+                    </div>
+                    `
+                },
+                "width": "15%"
+            }            
+        ]
+
+    });
+    
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Do you want to Delete this ?',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Yes',
+        denyButtonText: `No`,
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+            $.ajax({
+                'url': url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                },
+
+            })
+            Swal.fire('product deleted successfully')
+        } else if (result.isDenied) {
+            Swal.fire('product don\'t deleted', '', 'info')
+        }
+    })
+}
